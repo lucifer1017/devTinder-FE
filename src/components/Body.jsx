3559e19cdbee9dde2cb9a93706c5f1369dbdf415
lucifer1017 <lucifergrey1017@gmail.com>
2024-12-01 02:07:58 +0530

    Redux, constants

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
+import { useSelector } from "react-redux";
 
 const Body = () => {
+  const user = useSelector((store) => store.user);
   return (
     <div>
       <NavBar />
       <Outlet />
-      <Footer />
+      {user && <Footer />}
     </div>
   );
 };
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,29 @@
 import axios from "axios";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleLogin = async () => {
     try {
-      const res = axios.post(
-        "http://localhost:8000/login",
+      const res = await axios.post(
+        BASE_URL + "/login",
         {
           email,
           password,
         },
         { withCredentials: true }
       );
+
+      dispatch(addUser(res.data));
+      navigate("/");
     } catch (error) {
       console.error(error.message);
     }
